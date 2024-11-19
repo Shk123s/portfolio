09713b7e24ebc0c6c690e138cdd34da1709cc81b
shaqeebSk <shaqeeb@pentagoat.com>
2024-11-19 18:49:04 +0530

    hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
+import Hero from "./components/Hero";
 import Navbar from "./components/navbar";
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center h-screen w-full">
-        <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
-        <Navbar/>
+      <div className="overflow-x-hidden text-stone-300 antialiased">
+        <div className="fixed inset-0 -z-10">
+          <div className="relative h-full w-full">
+            <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
+            </div>
+          </div>
         </div>
-
+            <div className="container mx-auto px-8 ">
+              <Navbar />
+              <Hero/>
+            </div>
       </div>
     </>
   );
 }
 
 export default App;
+
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,65 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaDesktop } from "react-icons/fa";
 
-const navbar = () => {
+const Navbar = () => {
   return (
-    <div className='text-white '>
-     <div className="mx-2">
-        <img 
-          src='https://cdn-icons-png.freepik.com/256/12452/12452507.png?ga=GA1.1.1575563360.1730722946&semt=ais_hybrid' 
-          alt="Navbar Icon" 
-        />
-    </div>
-    </div> 
-  )
-}
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        {/* <img
+          src="https://cdn-icons-png.freepik.com/256/12452/12452507.png?ga=GA1.1.1575563360.1730722946&semt=ais_hybrid"
+          alt="Navbar Icon"
+          className="mx-2"
+          width={50}
+          height={33}
+        /> */} 
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn" 
+          
+        >
+          <FaDesktop  size={24}  />
+        
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://linkedin.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://instagram.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Instagram"
+        >
+          <FaInstagram />
+        </a>
+        <a
+          href="https://twitter.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="Twitter"
+        >
+          <FaTwitter />
+        </a>
+      </div>
+    </nav>
+  );
+};
 
-export default navbar
+export default Navbar;
